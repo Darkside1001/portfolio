2fde79c5ce944f313d87db486c3c898a325eea8d
RIO <ekofoisaac8@gmail.com>
2025-01-31 10:57:29 +0100

    V1.1

---
+++ script.js
@@ -0,0 +1,10 @@
+// Smooth scroll pour les liens du menu
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
