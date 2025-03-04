025dc5097a488e5d0ca8dd5bd036319e6ea669d8
RIO <ekofoisaac8@gmail.com>
2025-03-04 17:14:37 +0100

    `Update portfolio website: change title, add service container, modify styles and script`

--- script.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("name").textContent = "EKOFO BOTAY ISAAC";
-    document.getElementById("title").textContent = "Développeur Full Stack";
-    document.getElementById("contact").textContent = "ekofoisaac8@gmail.com";
-});
