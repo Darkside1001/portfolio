681d7c124b37437b9585f45caa6d114f2133bb1e
RIO <ekofoisaac8@gmail.com>
2025-03-07 09:40:54 +0100

    V11

--- Footer.js
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-    return (
-      <footer className="text-center p-4 bg-gray-100 dark:bg-gray-900">
-        <p className="text-gray-600 dark:text-gray-300">© 2025 Mon Portfolio</p>
-      </footer>
-    );
-  }
--- components/Navbar.js
+++
@@ -1,9 +0,0 @@
-import { PropsWithChildren } from 'react';
-import cn from 'classnames';
-
-
-import { PropsWithChildren } from "react";
-
-export const Section = ({ className = "", children }: PropsWithChildren<{ className?: string }> ) => {
-  return <section className={className}>{children}</section>;
-};
